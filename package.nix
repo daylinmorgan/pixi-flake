7954c1a7c602b94c54d3608f065951170adeeb64
Daylin Morgan <me@dayl.in>
2024-06-06 09:56:29 -0500

    bump v0.23

--- package.nix
+++ package.nix
@@ -11,26 +11,27 @@
   testers,
   pixi,
   pixi-source,
+  ...
 }:
 
 rustPlatform.buildRustPackage rec {
   pname = "pixi";
-  version = "0.22.0";
+  version = "0.23.0";
 
   src = fetchFromGitHub {
     owner = "prefix-dev";
     repo = "pixi";
     rev = "v${version}";
-    hash = "sha256-fTASWWvqaBn4XH7FL0fhdSphKFKhIOddKwsbDmln1g8=";
+    hash = "sha256-uLlScqcTU9nnBQiufRo+eee1TNyP5R52oI1jUaH6L1M=";
   };
 
   cargoLock = {
     lockFile = "${pixi-source}/Cargo.lock";
     outputHashes = {
-        "async_zip-0.0.17" = "sha256-Q5fMDJrQtob54CTII3+SXHeozy5S5s3iLOzntevdGOs=";
-        "cache-key-0.0.1" = "sha256-XsBTfe2+J5CGdjYZjhgxiP20OA7+VTCvD9JniLOjhKs=";
-        "pubgrub-0.2.1" = "sha256-sqC7R2mtqymYFULDW0wSbM/MKCZc8rP7Yy/gaQpjYEI=";
-      };
+      "async_zip-0.0.17" = "sha256-Q5fMDJrQtob54CTII3+SXHeozy5S5s3iLOzntevdGOs=";
+      "cache-key-0.0.1" = "sha256-fbaIeM4wgwi/3S8s9KRux8wKe6FbCBNPZFPekmrq2jA=";
+      "pubgrub-0.2.1" = "sha256-sqC7R2mtqymYFULDW0wSbM/MKCZc8rP7Yy/gaQpjYEI=";
+    };
   };
 
   nativeBuildInputs = [
@@ -58,30 +59,7 @@ rustPlatform.buildRustPackage rec {
     OPENSSL_NO_VENDOR = 1;
   };
 
-  # There are some CI failures with Rattler. Tests on Aarch64 has been skipped.
-  # See https://github.com/prefix-dev/pixi/pull/241.
-  # doCheck = !stdenv.isAarch64;
   doCheck = false;
-
-  preCheck = ''
-    export HOME="$(mktemp -d)"
-  '';
-
-  checkFlags = [
-    # Skip tests requiring network
-    "--skip=add_channel"
-    "--skip=add_functionality"
-    "--skip=add_functionality_os"
-    "--skip=add_functionality_union"
-    "--skip=add_pypi_functionality"
-    "--skip=test_alias"
-    "--skip=test_cwd"
-    "--skip=test_compressed_mapping_catch_missing_package"
-    "--skip=test_incremental_lock_file"
-    "--skip=test_purl_are_added_for_pypi"
-
-    "--skip=test_task_with_env" # `/usr/bin/env` required
-  ];
 
   postInstall = ''
     installShellCompletion --cmd pixi \

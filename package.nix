05405964d6639f1a32e4c81d453717f70327c093
Daylin Morgan <me@dayl.in>
2024-12-19 10:56:13 -0600

    pixi: 0.39.1 -> 0.39.3
    Diff: https://github.com/prefix-dev/pixi/compare/v0.39.1...v0.39.3

--- package.nix
+++ package.nix
@@ -13,13 +13,13 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "pixi";
-  version = "0.39.1";
+  version = "0.39.3";
 
   src = fetchFromGitHub {
     owner = "prefix-dev";
     repo = "pixi";
     rev = "v${version}";
-    hash = "sha256-queJy6xJcUOv3jFHBuC3i9s4OnNBC5bxwV9FCy3mCBI=";
+    hash = "sha256-QDnPXE6YY3AGUFtd6JRQVBAllO+acBBZ2yX+uNcBT7c=";
   };
 
   cargoLock = {
@@ -28,7 +28,7 @@ rustPlatform.buildRustPackage rec {
       "async_zip-0.0.17" = "sha256-VfQg2ZY5F2cFoYQZrtf2DHj0lWgivZtFaFJKZ4oyYdo=";
       "pubgrub-0.2.1" = "sha256-zusQxYdoNnriUn8JCk5TAW/nQG7fwxksz0GBKEgEHKc=";
       "tl-0.7.8" = "sha256-F06zVeSZA4adT6AzLzz1i9uxpI1b8P1h+05fFfjm3GQ=";
-      "uv-auth-0.0.1" = "sha256-iiIbSya+SUM7Xk+lV7h7Zyb1WMvc1IAHxCAylWmKaKU=";
+      "uv-auth-0.0.1" = "sha256-0tbaZ7xopQM0+nyLPiAH2zpS7rbWaIcT+f273ujbcQY=";
     };
   };
 

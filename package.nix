7c0d71163be7ea51da7f9d83aa4cc89f6450d124
Daylin Morgan <me@dayl.in>
2025-02-12 17:07:48 -0600

    pixi: 0.41.0 -> 0.41.3
    Diff: https://github.com/prefix-dev/pixi/compare/v0.41.0...v0.41.3

--- package.nix
+++ package.nix
@@ -13,22 +13,23 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "pixi";
-  version = "0.41.0";
+  version = "0.41.3";
 
   src = fetchFromGitHub {
     owner = "prefix-dev";
     repo = "pixi";
     rev = "v${version}";
-    hash = "sha256-QZHDH2UZmDC4u6TdS0mPzlJG5fdugXOXZG3eq2DupoA=";
+    hash = "sha256-gQe53kQTUMgEo9biTBV7fNGpZ6CjsBAOsXerfuVgXX8=";
   };
 
   cargoLock = {
     lockFile = "${src}/Cargo.lock";
     outputHashes = {
       "async_zip-0.0.17" = "sha256-VfQg2ZY5F2cFoYQZrtf2DHj0lWgivZtFaFJKZ4oyYdo=";
-      "pubgrub-0.2.1" = "sha256-zusQxYdoNnriUn8JCk5TAW/nQG7fwxksz0GBKEgEHKc=";
+      "pubgrub-0.3.0-alpha.1" = "sha256-FF10Ia2fvBIP/toxnjh/bqjHazFDChMd2qQzASGZLiM=";
       "tl-0.7.8" = "sha256-F06zVeSZA4adT6AzLzz1i9uxpI1b8P1h+05fFfjm3GQ=";
-      "uv-auth-0.0.1" = "sha256-iiIbSya+SUM7Xk+lV7h7Zyb1WMvc1IAHxCAylWmKaKU=";
+      "uv-auth-0.0.1" = "sha256-EWm1sjmDAmMQoGoRqgtFMlXwi8n/iCdahsoRERhhulc=";
+      "version-ranges-0.1.1" = "sha256-zusQxYdoNnriUn8JCk5TAW/nQG7fwxksz0GBKEgEHKc=";
     };
   };
 

299cfc650ea00b2faee22b60295bd3a32c2cb8e0
Daylin Morgan <me@dayl.in>
2025-02-28 07:32:42 -0600

    pixi: 0.42.0

--- package.nix
+++ package.nix
@@ -13,32 +13,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "pixi";
-  version = "0.41.3";
+  version = "0.42.0";
 
   src = fetchFromGitHub {
     owner = "prefix-dev";
     repo = "pixi";
     rev = "v${version}";
-    hash = "sha256-gQe53kQTUMgEo9biTBV7fNGpZ6CjsBAOsXerfuVgXX8=";
+    hash = "sha256-IT+USqTJW8HOfnY6TvW0cGFpDEd1/N46C+a0bQi88Ss=";
   };
-
-  cargoLock = {
-    lockFile = "${src}/Cargo.lock";
-    outputHashes = {
-      "async_zip-0.0.17" = "sha256-VfQg2ZY5F2cFoYQZrtf2DHj0lWgivZtFaFJKZ4oyYdo=";
-      "pubgrub-0.3.0-alpha.1" = "sha256-FF10Ia2fvBIP/toxnjh/bqjHazFDChMd2qQzASGZLiM=";
-      "tl-0.7.8" = "sha256-F06zVeSZA4adT6AzLzz1i9uxpI1b8P1h+05fFfjm3GQ=";
-      "uv-auth-0.0.1" = "sha256-EWm1sjmDAmMQoGoRqgtFMlXwi8n/iCdahsoRERhhulc=";
-      "version-ranges-0.1.1" = "sha256-zusQxYdoNnriUn8JCk5TAW/nQG7fwxksz0GBKEgEHKc=";
-    };
-  };
-
-  # The following packages are duplicated which is not supported by buildRustPackage:
-  # - version-ranges
-  #
-  # postPatch = ''
-  #   cp ${./Cargo.lock} Cargo.lock
-  # '';
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-0+b+OjefaPMzplw/S8jEB8uc9n2NXNrUBKcgzxzAhnc=";
 
   nativeBuildInputs = [
     pkg-config
